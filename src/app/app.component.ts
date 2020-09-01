f77a359a752b4fbf2c19917792c99f423fba42fe
Aura Jimenez <auracristina.jimenezg@gmail.com>
2020-08-31 20:21:19 -0500

    Inicio temas de estructuras de control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Product } from './product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'store';
+  items = ['nicolas', 'julian', 'perez'];
+
+  products: Product[] = [
+    {
+      id: '1',
+      title: 'Camiseta',
+      price: 8000,
+      description: 'una camisetota'
+    },
+    {
+      id: '2',
+      title: 'Pantacha',
+      price: 5000,
+      description: 'una pantacha'
+    },
+    {
+      id: '3',
+      title: 'Cucos',
+      price: 1000,
+      description: 'unos cucos'
+    },
+    {
+      id: '4',
+      title: 'Tanga',
+      price: 8000,
+      description: 'una tanga roja'
+    }
+  ];
+
+  // tslint:disable-next-line: typedef
+  addItem() {
+    this.items.push('nuevo item');
+  }
+
+  // tslint:disable-next-line: typedef
+  deleteItem(index: number){
+    this.items.splice(index, 1);
+  }
 }
